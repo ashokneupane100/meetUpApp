37ded17eb9be36c8581c20b4e52bb5df5a26b5ff
Ashok Neupane <ashokabrother@gmail.com>
2025-05-26 07:02:07 +0545

    styles with NativeWind #31.56

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,24 @@
 import { Stack } from 'expo-router';
-
-import { View } from 'react-native';
+import Feather from '@expo/vector-icons/Feather';
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+import { Text, View,Image } from 'react-native';
 
 export default function Home() {
   return (
     <>
       <Stack.Screen options={{ title: 'Events' }} />
+      <Text>Wed 13, Sep · 1930 CET  </Text>
+      {/* <Text style={{fontSize:30,fontWeight:"bold"}}>This is the title</Text> */}
+
+      <Text className='text-2xl font-bold'>This is the title</Text>
+      <Text>City Hall</Text>
+      {/* Event Image */}
+      <Image source={{uri:"https://notjustdev-dummy.s3.us-east-2.amazonaws.com/images/1.jpg"}} style={{width:200,height:200}} />
+
+      {/* Footer */}
+      <Text>16 going</Text>
+      <Feather name="share" size={18} color="black"/>
+      <FontAwesome name="bookmark" size={24} color="black" />
     </>
   );
 }
