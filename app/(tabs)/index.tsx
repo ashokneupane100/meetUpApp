d48b90d3c348d299d55ae789e85de54fde569670
Ashok Neupane <ashokabrother@gmail.com>
2025-05-26 13:09:25 +0545

    Adding the FlatList

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,25 +1,18 @@
 import { Stack } from 'expo-router';
-import { View } from 'react-native';
-import EventListItem  from '@/components/EventListItem';
-import events from "@/assets/events.json"
-
+import { View, FlatList } from 'react-native';
+import EventListItem from '@/components/EventListItem';
+import events from '@/assets/events.json';
 
 export default function Home() {
   return (
     <>
       <Stack.Screen />
-
-      {/* Event Cards Container */}
-      <View className="mx-2 my-2 gap-2">
-      
-
-        <EventListItem event={events[0]} />
-         <EventListItem event={events[1]} />
-          <EventListItem event={events[2]} />
-           <EventListItem event={events[3]} />
-            <EventListItem event={events[4]} />
-             <EventListItem event={events[5]} />
-      </View>
+      <FlatList
+       
+        data={events}
+        renderItem={({ item }) => <EventListItem event={item} />}
+         className="bg-white"
+      />
     </>
   );
 }
